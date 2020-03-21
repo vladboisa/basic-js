9aa06e11e1051759614529439829cd0d8ab4222d
VladBoisa <60569670+vladboisa@users.noreply.github.com>
2020-03-21 11:46:31 +0300

    feat: add countcat

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    let countcat = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === "^^") {
+                countcat += 1;
+            } else {
+                continue;
+            }
+        }
+    }
+    return countcat;
 };
