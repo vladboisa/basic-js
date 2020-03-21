48b4b514fd3eb499729176e732bb35550334a85e
VladBoisa <60569670+vladboisa@users.noreply.github.com>
2020-03-21 20:43:49 +0300

    feat: add transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,24 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (arr.length === undefined) {
+        throw Error;
+    }
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '--discard-next') {
+            i++;
+        } else if (arr[i] == '--discard-prev') {
+            newArr.pop();
+        } else if (arr[i] == '--double-next') {
+            if (arr.length - 1 > i) {
+                newArr.push(arr[i + 1]);
+            }
+        } else if (arr[i] == '--double-prev') {
+            if (i > 0) {
+                newArr.push(arr[i - 1]);
+            }
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
